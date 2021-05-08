d44eb71668adfb7259f2540e2be396c68acfc326
bbenligiray <bbenligiray@gmail.com>
2021-05-08 14:19:01 +0300

    Update EVMScript build procedure

--- main.js
+++ main.js
@@ -12,30 +12,53 @@ const votingApp2Address = '0x307b6d0500588809a03f60a60055656731ce72a6';
 const agentApp1Address = '0x7452c80849d1b1c4e09afea81e419251ea78d475';
 const agentApp2Address = '0x8d706d288a8e29b01362b44c3c2220f997574e4a';
 
-const encodedForwardSignature = encodeFunctionSignature('forward(bytes)');
 const encodedExecuteSignature = encodeFunctionSignature('execute(address,uint256,bytes)')
 
 const targetContractAddress = '0x0E7FdA608937489f410377745dAE331d14965bB0';
 const targetFunctionSignature = 'transfer(address,uint256)'
 const targetValue = '123';
-const targetParamters = ['0x07b589f06bD0A5324c4E2376d66d2F4F25921DE1', '456'];
-const metaData = "www.myproposaljustification.com";
+const targetParameters = ['0x07b589f06bD0A5324c4E2376d66d2F4F25921DE1', '456'];
+const metaData = 'www.myproposaljustification.com';
 
 async function main() {
-  const encodedTargetFunctionSignature = encodeFunctionSignature(targetFunctionSignature);
-  const encodedTargetParameters = ethers.utils.defaultAbiCoder.encode(targetFunctionSignature.substring(targetFunctionSignature.indexOf('(') + 1, targetFunctionSignature.indexOf(')')).split(','), targetParamters);
-  const executeCallData = encodedExecuteSignature + ethers.utils.defaultAbiCoder.encode(["address", "uint256", "bytes"], [targetContractAddress, targetValue, encodedTargetFunctionSignature + encodedTargetParameters.substring(2)]).substring(2);
-
-  const emvScriptSpecId = '0x00000001';
-  const executeCallDataBytesLength = ethers.utils.hexZeroPad(ethers.BigNumber.from(executeCallData.substring(2).length / 2).toHexString(), 4);
-  const forwardEvmScript = encodedForwardSignature + ethers.utils.defaultAbiCoder.encode(['bytes'], [emvScriptSpecId + agentApp2Address.substring(2) + executeCallDataBytesLength.substring(2) + executeCallData.substring(2)]).substring(2);
-
+  const parameterTypes = targetFunctionSignature
+    .substring(
+      targetFunctionSignature.indexOf('(') + 1,
+      targetFunctionSignature.indexOf(')')
+    )
+    .split(',');
+  const encodedTargetParameters = ethers.utils.defaultAbiCoder.encode(
+    parameterTypes,
+    targetParameters
+  );
+  const callData =
+    encodedExecuteSignature +
+    ethers.utils.defaultAbiCoder
+      .encode(
+        ['address', 'uint256', 'bytes'],
+        [
+          targetContractAddress,
+          targetValue,
+          encodeFunctionSignature(targetFunctionSignature) +
+            encodedTargetParameters.substring(2),
+        ]
+      )
+      .substring(2);
+  const callDataLengthInBytes = ethers.utils.hexZeroPad(
+    ethers.BigNumber.from(callData.substring(2).length / 2).toHexString(),
+    4
+  );
+  const evmScript =
+    '0x00000001' +
+    agentApp2Address.substring(2) +
+    callDataLengthInBytes.substring(2) +
+    callData.substring(2);
+  
   const votingAppAbi = [
-    //"function newVote(bytes _executionScript, string _metadata) external returns (uint256 voteId)",
-    "function newVote(bytes _executionScript, string _metadata, bool _castVote, bool _executesIfDecided) external returns (uint256 voteId)"
+    'function newVote(bytes _executionScript, string _metadata, bool _castVote, bool _executesIfDecided) external returns (uint256 voteId)'
   ];
   const votingApp2 = new ethers.Contract(votingApp2Address, votingAppAbi, wallet);
-  await votingApp2.newVote(forwardEvmScript, `${targetFunctionSignature} ${metaData}`, true, true, { gasLimit: 1000000 });
+  await votingApp2.newVote(evmScript, `${targetFunctionSignature} ${metaData}`, true, true, { gasLimit: 1000000 });
 }
 
 main();
